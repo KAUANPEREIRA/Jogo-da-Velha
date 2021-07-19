afbb839005ec8657107742d0acd4110e32e46351
KAUANSANTOS <66709503+KAUANPEREIRA@users.noreply.github.com>
2021-07-19 10:43:11 -0300

    atualização
    Atualização HTML.

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoade',()=>{
+
+
+let pecas = document.querySelectorAll(".peca")
+peca.innerHTML= `i` 
+pecas.forEach((peca)=>{
+    peca.addEventListener('click',clicou)
+
+})
+
+})
+
+function clicou(e){
+    console.log(e.target)
+}
+
---
+++ script/game.js
@@ -0,0 +1,7 @@
+//criação do tabuleiro do jogo
+let tabuleiro =['','','','','','','','','']
+// jogador sera 0 para o jogador 1 , e 1 para o jogador 2
+
+let jogador = 0
+let simbolos = ['o','x']
+
