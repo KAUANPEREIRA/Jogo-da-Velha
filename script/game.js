db824867e82683c60db144302a526e8dccccf62f
KAUANSANTOS <66709503+KAUANPEREIRA@users.noreply.github.com>
2021-07-20 11:56:18 -0300

    Adicionando ações e simbolos
    Adicionando e modificando ações e símbolos no jogo via Javascript.

--- script/game.js
+++ script/game.js
@@ -1,7 +1,19 @@
 //criação do tabuleiro do jogo
-let tabuleiro =['','','','','','','','','']
+let board=['','','','','','','','','']
 // jogador sera 0 para o jogador 1 , e 1 para o jogador 2
 
-let jogador = 0
-let simbolos = ['o','x']
+let playerTime = 0
+let simbols = ['o','x']
+
+function handleMove(position){
+ 
+board[position]= simbols[playerTime]
+ if(playerTime==0){
+     playerTime=1
+ }else{
+     playerTime=0
+ }
+
+
+}
 
